04376d59ba5cb69027c6e53c474a2ca8bffbc6ba
Shrivastav0yash <yashshrivastav8938@gmail.com>
2026-03-17 02:25:50 +0530

    git commit -m "JWT Setup with jwtutil and jwtfilter"

--- src/main/java/com/springboot/journal_app/controller/PublicController.java
+++ src/main/java/com/springboot/journal_app/controller/PublicController.java
@@ -2,27 +2,60 @@ package com.springboot.journal_app.controller;
 
 
 import com.springboot.journal_app.entity.User;
+import com.springboot.journal_app.service.UserDetailsServiceImpl;
 import com.springboot.journal_app.service.UserService;
+import com.springboot.journal_app.utils.JwtUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/public")
+@Slf4j
 public class PublicController {
 
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
     //Add new user
-    @PostMapping("/create-user")
-    public ResponseEntity<?> addNewUser(@RequestBody User newUser){ //localhost:8080/users
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@RequestBody User newUser){ //localhost:8080/users
         try{
             userService.saveUser(newUser);
             return new ResponseEntity<>(newUser, HttpStatus.CREATED);
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody User user){ //localhost:8080/users
+        try{
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            user.getUserName(), user.getPassword()
+                    ));
+            UserDetails userDetails = userDetailsService.loadUserByUsername(user.getUserName());
+            String jwt = jwtUtil.generateToken(userDetails.getUsername());
+            return new ResponseEntity<>(jwt, HttpStatus.OK);
+        } catch (Exception e) {
+            log.error("Wrong username password "+e);
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
     }
 
